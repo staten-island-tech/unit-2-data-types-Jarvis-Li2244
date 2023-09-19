d7ede15cc0107760be8382e44f5687fb9c161821
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2023-09-18 21:12:42 -0400

    yes

---
+++ madlibs copy.py
@@ -0,0 +1,18 @@
+adjective1 = "heroic"
+name1 = "Joe Biden"
+verb1 = "ran"
+place1 = "warzone"
+name2 = "the Pope"
+adjective2 = "inspiring"
+celebrity1 = "Elon Musk"
+superhero1 = "Superman"
+mythologicalcreature = "Chimera"
+food1 = "pizza"
+bodypart1 = "leg"
+country1 = "Iceland"
+verb2 = "tumbled"
+number1 = "ten"
+noise1 = "clang"
+
+
+print(f'It was at this moment, our {adjective1} {name1} {verb1} up to the podium radiating with civic pride. As he looked into the {place1}, he saw his best friend, {name2}, standing there proud to know that this {adjective2} man and not any other person was up there. Not {celebrity1} nor {superhero1} nor a {mythologicalcreature}. {name1} had never seen this many people at the {place1} and he knew it was his time to shine with maybe {number1} people. With one last bite of his {food1}, he raised his {bodypart1} high and proclaimed to all in the crowd. For {country1}!!! With that, the crowd roared and he silently {verb2} into the abyss with a loud {noise1}.')
---
+++ madlibs.py
@@ -0,0 +1,46 @@
+adjective1 = "heroic"
+name1 = "Joe Biden"
+verb1 = "ran"
+place1 = "warzone"
+name2 = "the Pope"
+adjective2 = "inspiring"
+celebrity1 = "Elon Musk"
+superhero1 = "Superman"
+food1 = "pizza"
+bodypart1 = "leg"
+country1 = "Iceland"
+verb2 = "tumbled"
+number1 = "ten"
+noise1 = "clang"
+
+print("It was at this moment, our ")
+print(adjective1)
+print(name1)
+print(verb1)
+print(" up to the podium radiating with civic pride. As he looked into the ")
+print(place1)
+print(", he saw his best friend, ")
+print(name2)
+print(", standing there proud to know that he knew this ")
+print(adjective2)
+print(" man and not any other person. Not ")
+print(celebrity1)
+print("nor")
+print(superhero1)
+print(name1) 
+print(" had never seen this many people at the ") 
+print(place1)
+print(" and he knew it was his time to shine. With one last bite of his ")
+print(food1)
+print(", he raised his " )
+print(bodypart1) 
+print(" high and proclaimed to all in the crowd.")
+print(" For ")
+print(country1)
+print("!!! With that, the crowd roared and he ")
+print(verb2)
+print( "into the abyss with a loud")
+print(noise1)
+
+
+f'It was at this moment, our {adjective1} {name1} {verb1} up to the podium radiating with civic pride. As he looked into the {place1}, he sa2w his best friend, {name2}, standing there proud to know that he knew this {adjective2} man and not any other person. Not {celebrity1} nor {superhero1}. {name1} had never seen this many people at the {place1} and he knew it was his time to shine with maybe {number1} people. With one last bite of his {food1}, he raised his {bodypart1} high and proclaimed to all in the crowd. For {country1}!!! With that, the crowd roared and he silently {verb2} into the abyss with a loud {noise1}.'
