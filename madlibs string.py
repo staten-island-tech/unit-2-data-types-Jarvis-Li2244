12a9d847d9b5f5da4993a9a25ab53f755689486c
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2023-09-19 13:35:05 -0400

    Update madlibs string.py

--- madlibs string.py
+++ madlibs string.py
@@ -14,5 +14,4 @@ verb2 = input("Enter a present tense verb: ")
 number1 = input("Enter a number: ")
 noise1 = input("Enter a noise: ")
 
-
 print(f'It was at this moment, our {adjective1} {name1} {verb1} up to the podium radiating with civic pride. As he looked into the {place1}, he saw his best friend, {name2}, standing there proud to know that this {adjective2} man and not any other person was up there. Not {celebrity1} nor {superhero1} nor a {mythicalCreature}. {name1} had never seen this many people at the {place1} and he knew it was his time to shine with maybe {number1} people right there. With one last bite of his {food1}, he raised his {bodypart1} high and proclaimed to all in the crowd. For {country1}!!! With that, the crowd roared and he silently started {verb2} into the abyss with a loud {noise1}.')
