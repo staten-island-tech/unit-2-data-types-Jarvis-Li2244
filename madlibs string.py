9804166a0d24c60f10b0e1cbd9d10d8ec0f5508b
Jarvis-Li2244 <144480728+Jarvis-Li2244@users.noreply.github.com>
2023-09-19 05:51:27 -0400

    MadLibs

--- madlibs string.py
+++ madlibs string.py
@@ -1,18 +1,18 @@
-adjective1 = "heroic"
-name1 = "Joe Biden"
-verb1 = "ran"
-place1 = "warzone"
-name2 = "the Pope"
-adjective2 = "inspiring"
-celebrity1 = "Elon Musk"
-superhero1 = "Superman"
-mythologicalcreature = "Chimera"
-food1 = "pizza"
-bodypart1 = "leg"
-country1 = "Iceland"
-verb2 = "tumbled"
-number1 = "ten"
-noise1 = "clang"
+adjective1 = input("Enter an adjective: ")
+name1 = input("Enter a name: ")
+verb1 = input("Enter a past tense verb: ")
+place1 = input("Enter a place: ")
+name2 = input("Enter a second name: ")
+adjective2 = input("Enter a second adjective: ")
+celebrity1 = input("Enter a celebrity: ")
+superhero1 = input("Enter a superhero: ")
+mythicalCreature = input("Enter a mythical creature: ")
+food1 = input("Enter a food: ")
+bodypart1 = input("Enter a body part: ")
+country1 = input("Enter a country: ")
+verb2 = input("Enter a present tense verb: ")
+number1 = input("Enter a number: ")
+noise1 = input("Enter a noise: ")
 
 
-print(f'It was at this moment, our {adjective1} {name1} {verb1} up to the podium radiating with civic pride. As he looked into the {place1}, he saw his best friend, {name2}, standing there proud to know that this {adjective2} man and not any other person was up there. Not {celebrity1} nor {superhero1} nor a {mythologicalcreature}. {name1} had never seen this many people at the {place1} and he knew it was his time to shine with maybe {number1} people. With one last bite of his {food1}, he raised his {bodypart1} high and proclaimed to all in the crowd. For {country1}!!! With that, the crowd roared and he silently {verb2} into the abyss with a loud {noise1}.')
+print(f'It was at this moment, our {adjective1} {name1} {verb1} up to the podium radiating with civic pride. As he looked into the {place1}, he saw his best friend, {name2}, standing there proud to know that this {adjective2} man and not any other person was up there. Not {celebrity1} nor {superhero1} nor a {mythicalCreature}. {name1} had never seen this many people at the {place1} and he knew it was his time to shine with maybe {number1} people right there. With one last bite of his {food1}, he raised his {bodypart1} high and proclaimed to all in the crowd. For {country1}!!! With that, the crowd roared and he silently started {verb2} into the abyss with a loud {noise1}.')
